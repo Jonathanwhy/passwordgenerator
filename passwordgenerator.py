582f175e52a11bf1067f107191c75442ba2f453b
Jonathan why <jonathan.m.yoon@gmail.com>
2025-02-06 19:01:54 +0000

    added the thing

---
+++ passwordgenerator.py
@@ -0,0 +1,29 @@
+import random
+import string
+import requests
+
+def random_character():
+    choices= string.ascii_letters + string.digits + string.punctuation
+    return random.choice(choices)
+
+desiredLength = input("How long do you want your password to be?\n")
+desiredLength = int(desiredLength)
+
+passwordLength= desiredLength
+
+def generate_strong_password():
+    password=""
+    for i in range(passwordLength):
+        password = password + random_character()
+    print(password)
+
+generate_strong_password()
+
+def fetch_word():
+    url = "https://random-word-api.herouapp.com/word?length=6"
+
+    response = requests.get(url)
+    word = response.json()[0]
+    return word
+
+print(fetch_word())
